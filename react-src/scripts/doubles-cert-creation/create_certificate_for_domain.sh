a3e593ef238fc0245ea29a667b3e0a36377363ec
Butch Howard <butch.howard@leadingagile.com>
2022-07-07 16:03:03 -0400

    Update scripts to match folder changes;
    remove client info from readme

--- react-src/scripts/doubles-cert-creation/create_certificate_for_domain.sh
+++ react-src/scripts/doubles-cert-creation/create_certificate_for_domain.sh
@@ -14,7 +14,7 @@ COMMON_NAME="${2:-$1}"
 SUBJECT="/C=CA/ST=None/L=NB/O=None/CN=${COMMON_NAME}"
 NUM_OF_DAYS=365
 
-BASEPATH="${LOCATION}/../../src/doubles"
+BASEPATH="${LOCATION}/../../react-src/doubles"
 KEY_FILE="${BASEPATH}/device.key"
 CSR_FILE="${BASEPATH}/device.csr"
 CRT_FILE="${BASEPATH}/device.crt"
