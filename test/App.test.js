d961c4f6d5bd102578bc069d7803547a22a824f3
Keith Dingle <keith.dingle@leadingagile.com>
2022-07-21 12:52:20 -0500

    elimitated multiple initializations of app

--- test/App.test.js
+++ test/App.test.js
@@ -1,23 +1,19 @@
 const expect = require('chai').expect
 const App = require('../src/App')
-const { missingDoubleConfig, doubleNotArrayConfig } = require('../test/badDoubler.config')
+const {missingDoubleConfig, doubleNotArrayConfig} = require('../test/badDoubler.config')
 
 describe('App', () => {
+    const app = new App()
     it('can exist', () => {
-        const app = new App()
-
         expect(app).to.be.ok
     })
 
     it('has a Server instance', () => {
-        const app = new App()
-
         expect(app.server).to.be.ok
     })
 
     describe('run()', () => {
         it('receives path to config file with one double', () => {
-            const app = new App()
             const configFile = {
                 doubles: [
                     {
@@ -38,35 +34,12 @@ describe('App', () => {
             expect(app.server.allDoubles.length).to.equal(1)
         })
 
-        it('throws malformed config error if config is missing doubles',() =>{
-            const app = new App()
-
+        it('throws malformed config error if config is missing doubles', () => {
             expect(() => app.run(missingDoubleConfig)).to.throw('Config file missing doubles')
         })
 
         it('throws config error if doubles not an array', () => {
-            const app = new App()
-
             expect(() => app.run(doubleNotArrayConfig)).to.throw('doubles is not an array')
         })
-
-        // it('throws config error if double does not have request property', () => {
-        //     const app = new App()
-        //     let config =  {
-        //         port: 404,
-        //         doubles: [{
-        //             url: 'http://some-url.com',
-        //             method: 'GET',
-        //             response: {
-        //
-        //             }
-        //         }
-        //         ]
-        //     }
-        //
-        //     expect(() => app.run)
-        // })
     })
-
-
 })
